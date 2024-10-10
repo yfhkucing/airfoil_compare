d4bfa302cfcecdaf0e07d9df156466f54cab4763
Yusuf <af003yusuf@gmail.com>
2024-10-10 20:39:52 +0700

    .

---
+++ Functions.py
@@ -0,0 +1,51 @@
+def AtmosPropStd(Href, PropertyID):
+    # This function calculates an atmospheric property based on the variable
+    # PropertyID at the given altitude H in ft, where:
+    # If PropertyID = 0 then return Temperature ratio
+    # If PropertyID = 1 then return Pressure ratio
+    # If PropertyID = 2 then return Density ratio
+    # If PropertyID = 10 then return Temperature
+    # If PropertyID = 11 then return Pressure
+    # If PropertyID = 12 then return Density
+    # If PropertyID = 13 then return Dynamics Vicosity
+    # If PropertyID = 14 then return Gravity
+    # If PropertyID = 15 then return Speed of Sound
+
+    # 1994 ATMOSPHERIC MODELING
+
+    # Calculation
+    R = 1 - 0.000022558 * Href
+    TempRatio = R
+    PressRatio = R ** 5.2561
+    DensRatio = R ** 4.2561
+
+    TempInt = 288.15 * TempRatio
+    PressInt = 101325 * PressRatio
+    DensInt = 1.225 * DensRatio
+
+    RatioDynVisco = (TempRatio ** 1.5) * ((288.15 + 110) / (TempInt + 110))
+    DynVisco = 0.00001789 * RatioDynVisco
+
+    GravityInt = (9.807 * (6371000 ** 2)) / ((6371000 + Href) ** 2)
+
+    SpeedSoundInt = 340.3 * ((TempInt / 288.15) ** 0.5)
+
+    # Output
+    if PropertyID == 0:
+        return TempRatio
+    elif PropertyID == 1:
+        return PressRatio
+    elif PropertyID == 2:
+        return DensRatio
+    elif PropertyID == 10:
+        return TempInt - 273.15
+    elif PropertyID == 11:
+        return PressInt
+    elif PropertyID == 12:
+        return DensInt
+    elif PropertyID == 13:
+        return DynVisco
+    elif PropertyID == 14:
+        return GravityInt
+    elif PropertyID == 15:
+        return SpeedSoundInt
---
+++ airfoil_compare.py
@@ -0,0 +1,179 @@
+import aerosandbox as asb
+import aerosandbox.numpy as np
+import matplotlib.pyplot as plt
+import aerosandbox.tools.pretty_plots as p
+import pandas as pd
+from ADRpy import atmospheres as at
+from Functions import AtmosPropStd as std
+import streamlit as st
+import matplotlib.pyplot as plt
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+
+with st.container(border=True):
+    st.subheader("reynold number calculator")
+
+    atmos= at.Atmosphere()
+
+    alt= st.number_input("Altitude (m)",value= 3048.0)
+    
+    velocity= st.number_input("v infinite (m/s)",value= 36.0)
+    col_aa,col_bb= st.columns(2)
+    with col_aa:
+        chord= st.number_input("Wing chord (m)")
+    with col_bb:
+        rho= atmos.airdens_kgpm3(alt)
+        dyn_vis= std(alt,13)
+        reynold_num= (rho*velocity*chord)/(dyn_vis)
+        st.write('Reynold number = '+ str(round(reynold_num,2)))
+
+with st.container():
+    col_a,col_b= st.columns(2)
+    with col_a:
+        airfoil1= st.text_input('Aerofoil input',"naca4412",key='aerofoil1')
+        airfoil2= st.text_input('Aerofoil input',"naca4415",key='aerofoil2')
+    with col_b:
+        airfoil3= st.text_input('Aerofoil input',"naca0009",key='aerofoil3')
+        airfoil4= st.text_input('Aerofoil input',"e214",key='aerofoil4')
+
+with st.container():
+    col1,col2= st.columns(2)
+    re_min= st.number_input('Reynold number',value=reynold_num,key='Re min',step=1.0) 
+    with col1:
+        alpha_min= st.number_input('Alpha min',value=-10,key='alpha min',step=1)
+    with col2:
+        alpha_max= st.number_input('Alpha max',value=18,key='alpha max',step=1)
+        # re_max= st.number_input('Re max',value=int(1e8),key='Re max',step=1)
+
+airfoil_array= [airfoil1,airfoil2,airfoil3,airfoil4]
+af_array= [asb.Airfoil(airfoil_array[i]) for i in range(len(airfoil_array))]
+
+with st.container():
+    col_1,col_2= st.columns(2)
+    with col_1:
+        fig, ax = plt.subplots(figsize=(6, 2))
+        st.pyplot(af_array[0].draw())
+        fig, bx = plt.subplots(figsize=(6, 2))
+        st.pyplot(af_array[1].draw())
+
+    with col_2:
+        fig, cx = plt.subplots(figsize=(6, 2))
+        st.pyplot(af_array[2].draw())
+        fig, dx = plt.subplots(figsize=(6, 2))
+        st.pyplot(af_array[3].draw())
+
+re = [re_min]
+alpha= [float(i) for i in range(alpha_min,alpha_max+1,1)]
+Alpha, Re = np.meshgrid(alpha, re)
+
+aero_flattened_array=[]
+aero_array=[]
+
+for i in range(len(airfoil_array)):
+    af=af_array[i]
+    aero_flattened = af.get_aero_from_neuralfoil(
+        alpha=Alpha.flatten(),
+        Re=Re.flatten(),
+        mach=0,
+        model_size="xxxlarge",
+    )
+    aero_flattened_array.append(aero_flattened)
+    Aero = {
+        key: value.reshape(Alpha.shape)
+        for key, value in aero_flattened.items()
+    }
+    aero_array.append(Aero)
+
+with st.container():    
+    #Cl-CD
+    fig, ax = plt.subplots()
+    for i in range(len(aero_array)):
+        line, = ax.plot(
+                        aero_array[i]["CD"][0, :],
+                        aero_array[i]["CL"][0, :],
+                        label= f" {af_array[i].name} Airfoil\n"
+                    )
+
+    plt.xscale('log')
+    plt.legend(loc='lower right')
+    # plt.axhline(y = cl_value[2], linestyle = '-', label= "Cl cruise ideal") 
+    # plt.axhline(y = cl_value[6], linestyle = '-', label= "Cl max clean") 
+    a= p.show_plot(
+                    title="$C_L$-$C_D$ Polar for Airfoil at RE = "+str(re_min),
+                    ylabel="Drag Coefficient $C_L$",
+                    xlabel="Lift Coefficient $C_D$",
+            )
+
+    st.pyplot(a)
+
+    # #Cl-AoA
+    # st.write(alpha)
+    fig, bx = plt.subplots()
+    for i in range(len(aero_array)):
+        line, = bx.plot(
+                            # aero_array[i]["CD"][0, :],
+                            np.array(alpha),
+                            np.array(aero_array[i]["CL"][0, :]),
+                            label= f" {af_array[i].name} Airfoil\n"
+                            # color=colors[0], alpha=0.8,
+                        )
+
+        # plt.xscale('log')
+    plt.legend(loc='lower right')
+    # plt.axhline(y = cl_value[2], linestyle = '-', label= "Cl cruise ideal") 
+    # plt.axhline(y = cl_value[6], linestyle = '-', label= "Cl max clean") 
+    b= p.show_plot(
+                        title="$AoA$-$C_L$ Polar for Airfoil at RE = "+str(re_min),
+                        xlabel="AoA",
+                        ylabel="Lift Coefficient $C_L$",
+                )
+
+    st.pyplot(b)
+
+    #Cd- AoA
+    fig, cx = plt.subplots()
+    for i in range(len(aero_array)):
+        line, = cx.plot(
+                            # aero_array[i]["CD"][0, :],
+                            np.array(alpha),
+                            np.array(aero_array[i]["CD"][0, :]),
+                            label= f" {af_array[i].name} Airfoil\n"
+                            # color=colors[0], alpha=0.8,
+                        )
+
+    # plt.xscale('log')
+    plt.legend(loc='lower right')
+
+    c= p.show_plot(
+                        title="$AoA$-$C_D$ Polar for Airfoil at RE = "+str(re_min),
+                        xlabel="AoA",
+                        ylabel="Drag Coefficient $C_D$",
+                )
+
+    st.pyplot(c)
+
+    #Cl/Cd - AoA
+    fig, cx = plt.subplots()
+    for i in range(len(aero_array)):
+        line, = cx.plot(
+                            # aero_array[i]["CD"][0, :],
+                            np.array(alpha),
+                            np.array(aero_array[i]["CL"][0, :]/aero_array[i]["CD"][0, :]),
+                            label= f" {af_array[i].name} Airfoil\n"
+                            # color=colors[0], alpha=0.8,
+                        )
+
+    # plt.xscale('log')
+    plt.legend(loc='lower right')
+    c= p.show_plot(
+                        title="$AoA$ - $C_L / C_D$ Polar for Airfoil at RE = "+str(re_min),
+                        xlabel="AoA",
+                        ylabel="$C_L / C_D$",
+                )
+
+    st.pyplot(c)
+    
+    # with st.container(border=True):
+    #  st.subheader('Airfoil requirements')
+    #  st.dataframe(airfoil_requirements)
